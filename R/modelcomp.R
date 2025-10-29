dc02b994e320d2cb24585ab6a729b2807e887f13
Jack Salole <jacksalole@gmail.com>
2025-10-29 07:15:31 -0400

    combined list in model comp for now. Also fits best model in this function to avoid the re-entering of same parameters

--- R/modelcomp.R
+++ R/modelcomp.R
@@ -3,14 +3,13 @@
 #' @param dataset A dataframe.
 #' @param Conc Unquoted column name of `dataset` of independent variable.
 #' @param Response Unquoted column name of `dataset` of dependant variable.
-#' @param models Optional. A string of model function names (i.e. `"LL.2"`).
-#' @param models_fx Optional. A list of model functions (i.e `LL.2`).
+#' @param model_list Optional. A list of named model function names (i.e. `model_list <- list("LL.4" = LL.4)`).
 #' @param metric Dictates which model parameter is used to compare model fit.
 #' @param list_obj Optional existing list object, used for integration with `runtoxdrc`.
 #'
 #' @importFrom drc LL.4 LN.4 W1.4 W2.4
 #'
-#' @returns A dataframe comparing model fits to the data. If list_obj is provided, stores this datafram in `list_obj$model_df`.
+#' @returns A fitetd drm model selected from the . If list_obj is provided, stores this datafram in `list_obj$model_df`.
 #'
 #' @export
 #'
@@ -21,48 +20,51 @@ modelcomp <- function(
   dataset,
   Conc,
   Response,
-  models = NULL,
-  models_fx = NULL,
-  metric = c("IC", "Res var", "Lack of fit", "no"),
+  model_list = NULL,
+  metric = c("IC", "Res var", "Lack of fit"),
   list_obj = NULL
 ) {
   # models needs a better arg name
-  if (is.null(models)) {
-    models <- (c(
-      "LL.4",
-      "LN.4",
-      "W1.4",
-      "W2.4"
-    ))
-  }
-
-  if (is.null(models_fx)) {
-    models_fx <- list(LL.4, LN.4, W1.4, W2.4)
+  if (is.null(model_list)) {
+    model_list <- list(
+  "LL.4" = LL.4,
+  "LN.4" = LN.4,
+  "W1.4" = W1.4,
+  "W2.4" = W2.4
+)
   }
 
   safe_model <- NULL
   fitted_model <- NULL
+  best_model_name <- NULL
+  best_model <- NULL
 
   ds <- dataset %>%
     dplyr::rename(Response = {{ Response }})
   dplyr::rename(Conc = {{ Conc }})
 
-  for (model in models_fx) {
+  for (model in model_list) {
     safe_model <- safe_drm(data = ds, Response ~ Conc, fct = model())
     if (!is.null(safe_model)) {
       fitted_model <- drm(data = ds, Response ~ Conc, fct = model())
-      model_df <- mselect2(fitted_model, models_fx, sorted = metric)
+      # if this line is a problem, might not accept named lists. Could just unname inputted list.
+      model_df <- mselect2(fitted_model, model_list, sorted = metric)
       break
     }
   }
 
   if (!is.null(model_df) && nrow(model_df) > 0) {
     model_df <- model_df[
-      !(model_df$model %in% c("model", "log_model")),
+      !(model_df$model %in% c("model")),
       ,
       drop = FALSE
     ]
     print(model_df)
+    best_model_name <- model_df$model[[1]]
+    
+    #if this line is the problem, add brackets directly to list entries.
+    best_model <- drm(data = ds, Response ~ Conc, fct = model_list[[best_model_name]]())
+    print(best_model)
   }
 
   if (!is.null(list_obj)) {
@@ -70,8 +72,10 @@ modelcomp <- function(
       stop("Provided list_obj must be a list.")
     }
     list_obj$model_df <- model_df
+    list_obj$best_model_name <- best_model_name
+    list_obj$model <- best_model
     return(list_obj)
   }
 
-  return(model_df)
+  return(best_model)
 }
